587236033584faceef071283480cbaafaf2af386
Umm e aiman <158820985+Ummeaiman288@users.noreply.github.com>
2024-06-15 19:30:49 +0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,8 @@
+var currentAmount=10000;
+var deposite=+prompt("enter how much do you want to add")
+currentAmount +=deposite
+console.log("deposite successfully" + currentAmount)
+
+var withdraw=-prompt("enter how much do you want?")
+currentAmount -=withdraw
+console.log(" deposite successfully" - currentAmount)
